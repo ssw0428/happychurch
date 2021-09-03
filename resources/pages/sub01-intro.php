6a89703ce01ce58297cbd1e673b4a194fc20fdd8
ssw0428 <ssw0428@naver.com>
2021-09-03 15:24:50 +0900

    book 올리기

--- resources/pages/sub01-intro.php
+++ resources/pages/sub01-intro.php
@@ -2,7 +2,65 @@
 include_once ($_SERVER['DOCUMENT_ROOT']."/resources/include/head.php");
 ?>
 
-<div>hi</div>
+<section id="sub01-paster-introduction" class="sub-con">
+    <div class="part01">
+        <div class="paster-img">
+            <img src="/resources/images/paster.jpg" alt="">
+        </div>
+        <div class="text-box">
+            <span class="text">표재근 목사</span>
+        </div>
+    </div>
+
+    <div class="part02">
+        <div class="title-box">
+            <span class="title">약력</span>
+        </div>
+        <div class="list">
+            <ul>
+                <li>
+                    <span class="word">신학박사(조직신학전공, Ph.D)</span>
+                </li>
+                <li>
+                    <span class="word">심리상담학 박사(D.C)</span>
+                </li>
+                <li>
+                    <span class="word">대한예수교장로회 행복한교회 담임목사</span>
+                </li>
+                <li>
+                    <span class="word">청소년 신앙공동체 비전스쿨 이사장</span>
+                </li>
+                <li>
+                    <span class="word">해피니스 심리상담센터 디렉터</span>
+                </li>
+                <li>
+                    <span class="word">세계한인기독교총연합회 상임회장</span>
+                </li>
+                <li>
+                    <span class="word">호서대, 동아인재대, 총신대 겸임교수</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="part03">
+        <div class="title-box">
+            <span class="title">목회 철학</span>
+        </div>
+        <div class="word-box">
+
+        </div>
+    </div>
+
+    <div class="part04">
+        <div class="title-box">
+            <span class="title">저서 소개</span>
+        </div>
+        <div class="word-box">
+            
+        </div>
+    </div>
+</section>
 
 <?php
 include_once ($_SERVER['DOCUMENT_ROOT']."/resources/include/foot.php");
